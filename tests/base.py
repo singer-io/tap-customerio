bf1002cb80a4c9edfdb01b5d75bf4dcd20c7d34c
Pagadala Narender <ext.pagadala.narender@globallogic.com>
2025-09-28 01:09:51 +0530

    change for the test

--- tests/base.py
+++ tests/base.py
@@ -111,14 +111,14 @@ class customerioBaseTest(BaseCase):
                 cls.API_LIMIT: 100
             },
             "snippets": {
-                cls.PRIMARY_KEYS: { "snippet_name" },
+                cls.PRIMARY_KEYS: { "name" },
                 cls.REPLICATION_METHOD: cls.INCREMENTAL,
                 cls.REPLICATION_KEYS: { "updated_at" },
                 cls.OBEYS_START_DATE: False,
                 cls.API_LIMIT: 100
             },
             "info": {
-                cls.PRIMARY_KEYS: { "id" },
+                cls.PRIMARY_KEYS: { "ip_addresses" },
                 cls.REPLICATION_METHOD: cls.FULL_TABLE,
                 cls.REPLICATION_KEYS: set(),
                 cls.OBEYS_START_DATE: False,
