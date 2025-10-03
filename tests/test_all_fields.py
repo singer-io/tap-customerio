a5ca2ab64eaede4fb0ffb1d558b1854e20b364d1
Pagadala Narender <ext.pagadala.narender@globallogic.com>
2025-10-03 16:10:11 +0530

    changes for the inti test

--- tests/test_all_fields.py
+++ tests/test_all_fields.py
@@ -18,6 +18,14 @@ class customerioAllFields(AllFieldsTest, customerioBaseTest):
         return "tap_tester_customerio_all_fields_test"
 
     def streams_to_test(self):
-        streams_to_exclude = {}
+        streams_to_exclude = {
+            'eps_suppression',
+            'subscription_center'
+            'sender_identities'
+            'broadcasts'
+            'customers'
+            'collections'
+            'messages'
+        }
         return self.expected_stream_names().difference(streams_to_exclude)
 
