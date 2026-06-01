12007c2c94c5c0b38fb6d54926c352ac8c18803e
mukeshbhatt18gl <mukesh.bhatt@qlik.com>
2026-06-01 15:34:51 +0530

    update base test

--- tests/base.py
+++ tests/base.py
@@ -18,6 +18,7 @@ class customerioBaseTest(BaseCase):
     in tap-tester tests. Shared tap-specific methods (as needed).
     """
     start_date = "2019-01-01T00:00:00Z"
+    IS_FORBIDDEN_STREAM = "is-forbidden-stream"
 
     @staticmethod
     def tap_name():
